5b6ef221f5792bcb5d82c6367d15778e552a8f0b
1f928 <81458569+1f928@users.noreply.github.com>
2021-09-13 09:07:52 -0500

    add solution

--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -4,10 +4,27 @@ import sys
 def main():
     next_biggest_number(sys.argv[1])
 
-
 def next_biggest_number(num):
-    #TODO: Implement me!
-    return 0
+    digits = [int(ch) for ch in str(num)]
+
+    # Reading right-to-left:
+    for i in range(len(digits) - 2, -1, -1):
+
+        # Find first digit lower than prior digit
+        if (digits[i] < digits[i + 1]):
+
+            # Swap low number with seen higher number closest in value
+            # And sort digits below the swap-line before reassembling
+            left_digits = digits[:i]
+            right_digits = sorted(digits[i:])
+            low = digits[i]
+            high = [n for n in right_digits if n > low][0]
+            right_digits.remove(high)
+            
+            return(int("".join(str(n) for n in (left_digits + [high] + right_digits))))
+
+    # Fail case
+    return -1
 
 if __name__ == "__main__":
     main()
